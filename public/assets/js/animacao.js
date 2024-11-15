23d41978ec23d28294f08250b7e4a4927e3da75b
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2024-11-15 01:11:25 -0300

    assets carousel - function henryque

--- public/assets/js/animacao.js
+++ public/assets/js/animacao.js
@@ -1,8 +1,11 @@
 // Carrosel Pag Home
 $(document).ready(function(){
     $('.box_carousel').slick({
+        dots: true,
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 2000
     });
 });
