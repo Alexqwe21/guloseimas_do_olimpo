575f762c41b36e69465d63ccdc14b668ffdd8569
alex jr <alexsandropimenta16@gmail.com>
2024-11-24 01:59:23 -0300

    responsivo 1024 768

--- public/assets/js/mobile-navbar.js
+++ public/assets/js/mobile-navbar.js
@@ -1,27 +1,20 @@
 class MobileNavbar {
-
-    constructor(mobileMenu, navlist, navlinks) {
-
-
+    constructor(mobileMenu, navlist, navlinks, closeBtn) {
         this.mobileMenu = document.querySelector(mobileMenu);
         this.navlist = document.querySelector(navlist);
         this.navlinks = document.querySelectorAll(navlinks);
+        this.closeBtn = document.querySelector(closeBtn);
         this.activeClass = "active";
 
-
-
         this.handleClick = this.handleClick.bind(this);
+        this.handleClose = this.handleClose.bind(this);
     }
-
-
 
     animateLinks() {
         this.navlinks.forEach((link, index) => {
-            console.log(index / 7);
             link.style.animation
                 ? (link.style.animation = "")
                 : (link.style.animation = `navLinkFade 0.5s ease forwards ${index * 0.1}s`);
-
         });
     }
 
@@ -29,36 +22,34 @@ class MobileNavbar {
         this.navlist.classList.toggle(this.activeClass);
         this.mobileMenu.classList.toggle(this.activeClass);
         this.animateLinks();
-    
-        // Controla o overflow do corpo
-     
     }
-    
 
+    handleClose() {
+        this.navlist.classList.remove(this.activeClass);
+        this.mobileMenu.classList.remove(this.activeClass);
+        this.animateLinks();
+    }
 
     addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick); 
-
-        
+        this.mobileMenu.addEventListener("click", this.handleClick);
+        if (this.closeBtn) {
+            this.closeBtn.addEventListener("click", this.handleClose);
+        }
     }
 
-
     init() {
-
-
         if (this.mobileMenu) {
             this.addClickEvent();
         }
-        return this
-
+        return this;
     }
-
 }
 
 const mobileNavbarInstance = new MobileNavbar(
     ".mobile-menu",
     ".nav-list",
     ".nav-list li",
+    ".close-btn"
 );
 
 mobileNavbarInstance.init();
