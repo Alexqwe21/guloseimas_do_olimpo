82836441cd4c9a7db952176712b505e11981afc4
henryque nonato <henryquenonatosilva@gmail.com>
2024-11-26 09:25:13 -0300

    Add pag "reserva"

--- public/assets/js/animacao.js
+++ public/assets/js/animacao.js
@@ -18,3 +18,38 @@ $(document).ready(function(){
     });
 
 });
+// Contator da tela "reserva"
+document.addEventListener("DOMContentLoaded", () => {
+    const numberDisplay = document.getElementById("number-display");
+    const incrementButton = document.getElementById("increment-button");
+    const decrementButton = document.getElementById("decrement-button");
+
+    let currentNumber = parseInt(numberDisplay.textContent.trim(), 10);
+    if (isNaN(currentNumber)) {
+        currentNumber = 1;
+        numberDisplay.textContent = currentNumber;
+    }
+
+    // Func.addNumber
+    incrementButton.addEventListener("click", () => {
+        currentNumber += 1;
+        numberDisplay.textContent = currentNumber;
+    });
+
+    // Fun.dimuirNumber(valor minimo = 1)
+    decrementButton.addEventListener("click", () => {
+        if (currentNumber > 1) {
+            currentNumber -= 1;
+            numberDisplay.textContent = currentNumber;
+        }
+    });
+});
+
+// favoritar produto
+document.addEventListener("DOMContentLoaded", () => {
+    const heartIcon = document.getElementById("heart-icon");
+
+    heartIcon.addEventListener("click", () => {
+        heartIcon.classList.toggle("favorited"); 
+    });
+});
